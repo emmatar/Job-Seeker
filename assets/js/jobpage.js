c53b55ca16df5d663745cd7da3d1dcd8ec6a3646
Elijah Matar <m.matar515@gmail.com>
2024-01-31 21:23:39 -0600

    creating first function to link api info

--- assets/js/jobpage.js
+++ assets/js/jobpage.js
@@ -1,3 +1,15 @@
-function heythere () {
-    alert("hey there")
+var api = "f5217bc497mshb6d8ca9af6d12ccp18ff3bjsnbe6c2937dff1";
+
+function base() {
+    var url = `https://job-salary-data.p.rapidapi.com/job-salary?X-RapidAPI-Host=job-salary-data.p.rapidapi.com&X-RapidAPI-Key=${apiKey}&job_title=developer&location=newyork`
+
+    fetch(url).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data){
+                console.log(data);
+            })
+        }
+    })
 }
+
+base();
