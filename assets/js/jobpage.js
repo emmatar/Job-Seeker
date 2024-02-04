83c987f8383c31e83ee16fb0409327ffb4226eca
Elijah Matar <m.matar515@gmail.com>
2024-02-03 21:33:26 -0600

    add script function for display

--- assets/js/jobpage.js
+++ assets/js/jobpage.js
@@ -1,31 +1,66 @@
-const jobResults = document.querySelector("#job-results")
-const oneJobCont = document.querySelector("#one-job-cont") 
-const averageSalary = document.querySelector("#average-salary-text")
 
-const testDataObj = {
-    position: "developer",
-    company: "google",
-    location: "kansasCity",
-    salary: "$73,000/year"
-}
+// this function links the data from the outer linkedIn.js file to this file.
+// "data" (the above parameter) is the actual info that the search will display
+linkedinJobSearch().then(function(data) {
+    displayJobSearchData(data)
+})
+ 
+function displayJobSearchData(jobInfo) {
+    // (jobInfo) is the data from the search.
+    // First, target the single "#job-post" container
+    // Then, create elements that will display the info we want the user to see
+    // Then, assign the elements with the info from the data (jobInfo) retrieved
+    for (let index = 0; index < jobInfo.length; index++) {
+    // The variable "singleJobInfo" will display information for a single job.
+    // Variables created to display data from LinkedIn Job Search API
+        const allJobsContainer = document.querySelector(".all-jobs-container")
+        const singleJobInfo = jobInfo[index];
+        const jobRole = document.createElement("h2");
+        const jobCompany = document.createElement("p");
+        const jobLocation = document.createElement("p");
+        const jobPostDate = document.createElement("p");
+        const jobCompanyUrlCont = document.createElement("div")
+        const jobCompanyUrl = document.createElement("a");
+        jobCompanyUrlCont.setAttribute('class', 'url');
+        const jobPostUrlCont = document.createElement("div");
+        const jobPostUrl = document.createElement("a");
+        jobPostUrlCont.setAttribute('class', 'url');
+        const singleJobPost = document.createElement("div");
+        singleJobPost.setAttribute('class', "single-job-post")
+        const here = document.createTextNode("Here")
+        
+    // Assigning the "loop" created elements to the corresponding data for display
+        jobRole.textContent = `Job Role: ${singleJobInfo.job_title}`;
+        jobCompany.textContent = `Company: ${singleJobInfo.company_name}`;
+        jobLocation.textContent = `Location: ${singleJobInfo.job_location}`;
+        jobPostDate.textContent = `Post Date: ${singleJobInfo.posted_date}`;
+        jobCompanyUrlCont.textContent = "Company-Url: "
+        jobPostUrlCont.textContent = "Job-Post-Url: "
+        jobPostUrl.title = "This is link"
+        jobPostUrl.href = singleJobInfo.job_url;
+        jobCompanyUrl.title = "This is link"
+        jobCompanyUrl.href = singleJobInfo.company_url;
+        console.log(singleJobInfo.job_url);
 
-// The empty parameter for this function will be the data retrieved from the Job Salary API
-// displayJobSalary () {
-    // averageSalary.textContent = {data};
-// }
 
-// The empty parameter for this function will be the data retrieved from the Job Search API
-function displayJobData () {
-    const jobType = testDataObj.position;
-
-    jobResults.textContent = `Search Results for: ${testDataObj.position}`
-    for (let index = 0; index < testDataObj.length; index++) {
-        const jobPosition = document.createElement("h3");
-        const jobCompany = document.createElement("h4");
-        const jobSalary = document.createElement("p");
-        const jobRemote = document.createElement("p");
-        const jobLink = document.createAttribute("a")
+        jobPostUrl.appendChild(here);
+        jobCompanyUrl.appendChild(here);
+        singleJobPost.append(jobRole)
+        singleJobPost.append(jobCompany)
+        singleJobPost.append(jobLocation)
+        singleJobPost.append(jobPostDate)
+        jobCompanyUrlCont.append(jobCompanyUrl)
+        singleJobPost.append(jobCompanyUrlCont)
+        jobPostUrlCont.append(jobPostUrl)
+        singleJobPost.append(jobPostUrlCont)
+        allJobsContainer.append(singleJobPost)
 
     }
+
 }
-displayJobData()
+
+
+
+
+
+linkedinJobSearch();
